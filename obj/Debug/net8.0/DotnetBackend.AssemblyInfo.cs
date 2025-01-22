40a2ed84f9912accf10936689a43485b0a76aeef
Caiuã Mello <105950039+caiua0800@users.noreply.github.com>
2025-01-22 01:47:39 -0300

    atualizei mongodb

--- obj/Debug/net8.0/DotnetBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b19c1465f6ba3932c4330b50185d77d2bbe7f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b828c86536c5fb445196eeae2b647cb9e1c545e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
